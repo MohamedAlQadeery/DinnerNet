25fd7f516746e80920f4e2ea1d7889a9640dee0f
Mohamed AlQadeery <moh9amad1@gmail.com>
2023-06-11 13:25:04 +0300

    Created MenuReviews /User aggregate root

--- DinnerNet.Domain/MenuReview/ValueObjects/MenuReviewId.cs
+++ DinnerNet.Domain/MenuReview/ValueObjects/MenuReviewId.cs
@@ -1,21 +1,37 @@
 using DinnerNet.Domain.Common.Models;
+using DinnerNet.Domain.Dinner.ValueObjects;
+using DinnerNet.Domain.Guest.ValueObjects;
+using DinnerNet.Domain.Menu.ValueObjects;
 
 namespace DinnerNet.Domain.MenuReview.ValueObjects;
 
 public sealed class MenuReviewId : ValueObject
 {
-    public Guid Value { get; }
+    public string Value { get; }
 
 
-    private MenuReviewId(Guid value)
+    private MenuReviewId(MenuId menuId, DinnerId dinnerId, GuestId guestId)
+    {
+        Value = $"MenuReview_{menuId.Value}_{dinnerId.Value}_{guestId.Value}";
+    }
+
+    private MenuReviewId(string value)
     {
         Value = value;
     }
 
-    public static MenuReviewId CreateUnique()
+    public static MenuReviewId Create(MenuId menuId, DinnerId dinnerId, GuestId guestId)
     {
-        return new MenuReviewId(Guid.NewGuid());
+        // TODO: enforce invariants
+        return new MenuReviewId(menuId, dinnerId, guestId);
     }
+
+    public static MenuReviewId Create(string value)
+    {
+        // TODO: enforce invariants
+        return new MenuReviewId(value);
+    }
+
     public override IEnumerable<object> GetEqualityComponents()
     {
         yield return Value;
